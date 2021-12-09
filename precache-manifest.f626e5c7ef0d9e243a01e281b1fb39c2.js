ddb52483d32bff799b416ca5b34ec8e5a4c53555
matthiasbigl <73336850+matthiasbigl@users.noreply.github.com>
2021-12-09 09:45:03 +0100

    Updates

---
+++ precache-manifest.f626e5c7ef0d9e243a01e281b1fb39c2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea7820a124b377b1ad15834a2be7259",
+    "url": "/webportfolio/index.html"
+  },
+  {
+    "revision": "10c84089fcab9cc47bb5",
+    "url": "/webportfolio/static/css/main.37300ba5.chunk.css"
+  },
+  {
+    "revision": "4033c46a58c6fa1f0bf4",
+    "url": "/webportfolio/static/js/2.fe4ed999.chunk.js"
+  },
+  {
+    "revision": "2aa2c05c2239026a2a1fbfdd082d1cae",
+    "url": "/webportfolio/static/js/2.fe4ed999.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10c84089fcab9cc47bb5",
+    "url": "/webportfolio/static/js/main.2c377be5.chunk.js"
+  },
+  {
+    "revision": "f79f2c7d035783fed9fa",
+    "url": "/webportfolio/static/js/runtime-main.667d8296.js"
+  },
+  {
+    "revision": "612bf5b2a237158833a196590338d4d6",
+    "url": "/webportfolio/static/media/Java-Developer-Skills.612bf5b2.jpg"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/webportfolio/static/media/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "544fa4f2678a8285eb88b8dfe503c90c",
+    "url": "/webportfolio/static/media/Poppins-ExtraBold.544fa4f2.ttf"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/webportfolio/static/media/Poppins-Light.f6ea751e.ttf"
+  },
+  {
+    "revision": "f61a4eb27371b7453bf5b12ab3648b9e",
+    "url": "/webportfolio/static/media/Poppins-Medium.f61a4eb2.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/webportfolio/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "4cdacb8f89d588d69e8570edcbe49507",
+    "url": "/webportfolio/static/media/Poppins-SemiBold.4cdacb8f.ttf"
+  },
+  {
+    "revision": "25cd0f688f815bc4f6ac2b71eb6278ba",
+    "url": "/webportfolio/static/media/Poppins-Thin.25cd0f68.ttf"
+  },
+  {
+    "revision": "2da9159058e472c3328c45eb4730c1b0",
+    "url": "/webportfolio/static/media/solid-color-image.2da91590.png"
+  }
+]);
